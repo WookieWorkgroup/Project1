cf63b15f271c068be0eb85650c41ab9f5c69b523
m1tche11d13 <dm574@mail.umkc.edu>
2016-02-14 00:06:45 -0600

    Edited Term.h for readability

--- Term.h
+++ Term.h
@@ -8,8 +8,11 @@ using namespace std;
 class Term
 {
 public:
+	//Constructors
 	Term(int c = 0, int e = 0);
 	Term(const Term &rhs);
+	
+	//Overloaded operators
 	void operator =(const Term &rhs);
 	friend ostream& operator<<(ostream& os, const Term& t);
 	friend Term operator +(const Term&lhs, const Term&rhs);
