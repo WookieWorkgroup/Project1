2bf07a00e37590c12c6c3719d06cab6dac9ee545
Chen Wang <cwrp3@mail.umkc.edu>
2016-02-15 16:51:52 -0600

    Finished the Polynomial.h and cpp

--- Polynomial.h
+++ Polynomial.h
@@ -9,6 +9,8 @@ class Polynomial
 public:
 	Polynomial();
 	void sort();
+	void clear();
+	bool is_empty();
 	friend Polynomial operator +(Polynomial& lhs, Polynomial& rhs);
 	friend ostream& operator<<(ostream& os, const Polynomial& P);
 	friend istream& operator>>(istream& is, Polynomial& P);
