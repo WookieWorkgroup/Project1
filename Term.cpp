d398020bdae59437c0dffbb992a4635ac8d62afd
m1tche11d13 <dm574@mail.umkc.edu>
2016-02-15 00:51:42 -0600

    Added overloading of the input operator for the Term class

--- Term.cpp
+++ Term.cpp
@@ -24,6 +24,61 @@ ostream& operator<<(ostream& os, const Term& t)
 	return os;
 }
 
+istream& operator>>(istream& is, Term& t)
+{
+	int negMult(1), coeff(0), exp(0);
+	stringstream ss;
+	
+	char c = is.get();
+	int val;
+
+	if (c == '-')
+	{
+		negMult = -1;
+		cout << "NEGATIVE\n";
+	}
+	else if (c == '+')
+	{
+		negMult = 1;
+		cout << "POSITIVE\n";
+	}
+	else
+	{
+		//loop until we hit the x
+		while (c != 'X' && c != 'x')
+		{
+			ss << c;
+			c = is.get();
+		}
+
+		//turn our string into an int
+		string::size_type sz;
+		coeff = stoi(ss.str(), &sz);
+		coeff *= negMult;
+
+		c = is.get(); //toss X
+		c = is.get(); //toss ^
+				
+		//clear the input from the coefficient
+		ss.str(string()); 
+
+		//iterate until the end of the line or the next term
+		while (c != '+' && c != '-' && c != '\n') 
+		{
+			ss << c;
+			c = is.get();
+		}
+		
+		//turn our string into an int
+		exp = stoi(ss.str(), &sz);
+
+		//modify the Term values
+		t.coefficient = coeff; 
+		t.exponent = exp;
+	}
+	return is;
+}
+
 Term operator +(const Term&lhs, const Term&rhs)
 {
 	//Assumes exponents are checked to be the same before this function is called.
--- Term.h
+++ Term.h
@@ -2,6 +2,7 @@
 #define TERM_H
 
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -12,9 +13,17 @@ public:
 	Term(int c = 0, int e = 0);
 	Term(const Term &rhs);
 	
+	//Getters & Setters
+	int getCoefficient() { return coefficient; }
+	int getExponent() { return exponent; }
+
+	void setCoefficient(int c){ coefficient = c; }
+	void setExponent(int e){ exponent = e; }
+
 	//Overloaded operators
 	void operator =(const Term &rhs);
 	friend ostream& operator<<(ostream& os, const Term& t);
+	friend istream& operator>>(istream& is, Term& t);
 	friend Term operator +(const Term&lhs, const Term&rhs);
 	bool operator > (Term &rhs);
 	bool operator >= (Term &rhs);
