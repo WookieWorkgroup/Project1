bc9331d047e07a0b49290e97a7d9ebe30ae6b8f1
m1tche11d13 <dm574@mail.umkc.edu>
2016-02-15 12:33:36 -0600

    Deleted Term class test functions from main

--- main.cpp
+++ main.cpp
@@ -47,38 +47,7 @@ using namespace std;
 
 int main()
 {
-	//Added in some quick tests to make sure that Term.h and Term.cpp work as expected...
-	//All functions work, feel free to delete whenever
 
-	Term a(1, 4);
-	Term b(2, 3);
-	Term c(3, 2);
-	Term d(4, 2);
-
-	//Greater than check
-	cout << "Expected: 1\tActual: " << (a > b) << endl;
-	cout << "Expected: 0\tActual: " << (b > a) << endl;
-	cout << "Expected: 0\tActual: " << (c > d) << endl << endl;
-
-	//Less than check
-	cout << "Expected: 0\tActual: " << (a < b) << endl;
-	cout << "Expected: 1\tActual: " << (b < a) << endl;
-	cout << "Expected: 0\tActual: " << (c < d) << endl << endl;
-
-	//Equal to check
-	cout << "Expected: 0\tActual: " << (a == b) << endl;
-	cout << "Expected: 1\tActual: " << (c == d) << endl << endl;
-
-	//I assume >= and <= work if the above three work.
-
-	//Addition check
-	cout << "Expected: 7X^2\tActual: " << (c+d) << endl << endl;
-
-	//Input and output check
-	cout << "Enter a term: ";
-	Term t;
-	cin >> t;
-	cout << t << endl << endl;
 
 	system("pause");
 	return 0;
